9d40ae876fd49629da7fbce2c911c37bb28a9240
NinnaHS <ninna.hirata@usp.br>
2017-10-30 23:35:42 -0200

    adicionando exercicio 1036.c

---
+++ 1036.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+ 
+    double a, b, c, r1, r2, delta;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    delta= (b*b) - (4*a*c);
+    if(a==0 || delta < 0)
+        printf("Impossivel calcular\n");
+    else
+    {
+        r1= (-b + sqrt(delta))/ (2*a);
+        r2= (-b - sqrt(delta))/ (2*a);
+        printf("R1 = %.5lf\nR2 = %.5lf\n",r1, r2);
+    }
+    return 0;
+    return 0;
+}
